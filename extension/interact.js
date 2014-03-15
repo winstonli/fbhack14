d9782311271a3ea368878fc2f2dd0c1a88258369
aicioara <primulandrei@yahoo.com>
2014-03-15 20:21:15 +0000

    Implemented push front. Inserting and pushing songs works fine

--- extension/interact.js
+++ extension/interact.js
@@ -1,7 +1,7 @@
-// x = document.body.style.backgroundColor="blue"
 
 
 function foo() {
+	x = document.body.style.backgroundColor="blue"
 	x = document.getElementById('rightCol')
 	x.parentNode.removeChild(x)
 }
