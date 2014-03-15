69d10bb9cef7170fb70be13261c52acbd7e00d48
Winston Li <Roxy@Lockheed>
2014-03-15 16:11:46 +0000

    nearly there

--- www/user_creation.php
+++ www/user_creation.php
@@ -3,8 +3,10 @@
 include_once(__DIR__ . "/../private/user_creation_request.php");
 
 $request = new UserCreationRequest();
-echo 'hi';
+echo "hi\n";
 $request->request();
+echo "hi2\n";
 $request->output();
+echo "done\n";
 
 ?>
