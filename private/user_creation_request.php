c38bc79e69fc9229b0cd16472b606a4578363105
Winston Li <Roxy@Lockheed>
2014-03-15 16:10:04 +0000

    got back

--- private/user_creation_request.php
+++ private/user_creation_request.php
@@ -2,19 +2,19 @@
 
 include_once __DIR__ . "/request.php";
 
-echo "got to bottom\n";
+echo "request fine\n";
 
 class UserCreationRequest extends Request {
 
-// 	public function request() {
-// 		$query = $this->db->request("SELECT count(*) FROM user.user");
-// 		if (!$query) {
-// 			return $this->error("failed");
-// 		}
-// 		$result = $query->fetch_assoc();
+	public function request() {
+		$query = $this->db->request("SELECT count(*) FROM user.user");
+		if (!$query) {
+			return $this->error("failed");
+		}
+		$result = $query->fetch_assoc();
 
-// 		return $this->success($result["count(*)"]);
-// 	}
+		return $this->success($result["count(*)"]);
+	}
 
 }
 
