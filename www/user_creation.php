297803a949ea68731cfce2be8237a0003636a358
Winston Li <Roxy@Lockheed>
2014-03-15 16:52:57 +0000

    more deubg comments

--- www/user_creation.php
+++ www/user_creation.php
@@ -2,14 +2,13 @@
 
 include_once __DIR__ . "/../private/user_creation_request.php";
 
-echo "start\n";
-
 $request = new UserCreationRequest($_POST["username"],
 								   $_POST["password"],
 								   $_POST["first_name"],
 								   $_POST["last_name"]);
-echo "constructed\n";
 $request->request();
+echo "requested\n";
 $request->output();
+echo "outputted\n";
 
 ?>
