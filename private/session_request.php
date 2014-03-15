3ed0d9ac0fdaf385da4a2b02c27da7ff12b24518
Winston Li <Roxy@Lockheed>
2014-03-15 16:43:29 +0000

    removed class body

--- private/session_request.php
+++ private/session_request.php
@@ -4,16 +4,16 @@ include_once __DIR__ . "/request.php";
 
 class SessionRequest extends Request {
 
-	private $session_token;
+	// private $session_token;
 
-	public function __construct($session_token) {
-		parent::__construct();
-		$this->session_token = $this->db->escape_string($session_token);
-	}
+	// public function __construct($session_token) {
+	// 	parent::__construct();
+	// 	$this->session_token = $this->db->escape_string($session_token);
+	// }
 
-	protected function valid_session() {
-		return true;
-	}
+	// protected function valid_session() {
+	// 	return true;
+	// }
 
 }
 
