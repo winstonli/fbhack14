0c46cc21e2ee1534003220e5a4c68c744ad52407
Winston Li <Roxy@Lockheed>
2014-03-15 15:57:53 +0000

    debugging

--- www/user_creation.php
+++ www/user_creation.php
@@ -2,9 +2,10 @@
 
 include_once __DIR__ . "/../private/user_creation_request.php";
 
-$request = new UserCreationRequest();
-echo 'hi';
-$request->request();
-$request->output();
+echo 'included';
+// $request = new UserCreationRequest();
+// echo 'hi';
+// $request->request();
+// $request->output();
 
 ?>
