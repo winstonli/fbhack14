7ef5dc044daa4cd637fe5d2e1eab111d3a39b9d3
Winston Li <Roxy@Lockheed>
2014-03-15 18:18:14 +0000

    playlist get

--- private/playlist/playlist_get_request.php
+++ private/playlist/playlist_get_request.php
@@ -16,8 +16,7 @@ class PlaylistGetRequest extends Request {
 			$this->playlist_id);
 
 		if (!$query) {
-			return $this->error("SELECT song_id, position, youtube_url FROM music.song WHERE playlist_id = " .
-			$this->playlist_id);
+			return $this->error(NULL);
 		}
 
 		$song_list = array();
--- www/api/playlist/get.php
+++ www/api/playlist/get.php
@@ -2,7 +2,7 @@
 
 include_once __DIR__ . "/../../../private/playlist/playlist_get_request.php";
 
-$request = new PlaylistGetRequest($_POST["playlist_get"]);
+$request = new PlaylistGetRequest($_POST["playlist_id"]);
 $request->request();
 $request->output();
 
