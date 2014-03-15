e12a0b8873d5d5364063899979ac2689256c4f66
Winston Li <Roxy@Lockheed>
2014-03-15 16:41:01 +0000

    trying to make sesion reuqest.

--- www/user_creation.php
+++ www/user_creation.php
@@ -1,8 +1,9 @@
 <?php
-
+echo "start\n";
 include_once __DIR__ . "/../private/user_creation_request.php";
-
+echo "included\n";
 $request = new UserCreationRequest($_POST["session_token"], $_POST["username"], $_POST["password"], $_POST["first_name"], $_POST["last_name"]);
+echo "created\n";
 $request->request();
 $request->output();
 
