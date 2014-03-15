25d4d6aa3c070e35def3c539a79bdded24c6b8fd
Winston Li <Roxy@Lockheed>
2014-03-15 16:35:12 +0000

    args

--- private/user_creation_request.php
+++ private/user_creation_request.php
@@ -4,6 +4,15 @@ include_once __DIR__ . "/request.php";
 
 class UserCreationRequest extends Request {
 
+	private $username;
+	private $password;
+	private $first_name;
+	private $last_name;
+
+	public function __construct($session_token, $username, $password, $first_name, $last_name) {
+		
+	}
+
 	public function request() {
 		$query = $this->db->query("SELECT count(*) FROM user.user");
 		if (!$query) {
--- www/user_creation.php
+++ www/user_creation.php
@@ -2,7 +2,7 @@
 
 include_once __DIR__ . "/../private/user_creation_request.php";
 
-$request = new UserCreationRequest();
+$request = new UserCreationRequest($_POST["username"], $_POST["password"], $_POST["first_name"], $_POST["last_name"]);
 $request->request();
 $request->output();
 
