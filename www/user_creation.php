529b797a5af54d90b758e0e246143b6685180475
Winston Li <Roxy@Lockheed>
2014-03-15 16:45:25 +0000

    laid out

--- www/user_creation.php
+++ www/user_creation.php
@@ -1,9 +1,12 @@
 <?php
-echo "start\n";
+
 include_once __DIR__ . "/../private/user_creation_request.php";
-echo "included\n";
-$request = new UserCreationRequest($_POST["session_token"], $_POST["username"], $_POST["password"], $_POST["first_name"], $_POST["last_name"]);
-echo "created\n";
+
+$request = new UserCreationRequest($_POST["session_token"],
+								   $_POST["username"],
+								   $_POST["password"],
+								   $_POST["first_name"],
+								   $_POST["last_name"]);
 $request->request();
 $request->output();
 
