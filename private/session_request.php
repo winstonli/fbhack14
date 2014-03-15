925908d5aa05da5a4e59ef0a2d34814d2def492d
Winston Li <Roxy@Lockheed>
2014-03-15 16:44:57 +0000

    request

--- private/session_request.php
+++ private/session_request.php
@@ -2,23 +2,19 @@
 
 include_once __DIR__ . "/request.php";
 
-echo "request included fine\n";
-
 abstract class SessionRequest extends Request {
 
-	// private $session_token;
+	private $session_token;
 
-	// public function __construct($session_token) {
-	// 	parent::__construct();
-	// 	$this->session_token = $this->db->escape_string($session_token);
-	// }
+	public function __construct($session_token) {
+		parent::__construct();
+		$this->session_token = $this->db->escape_string($session_token);
+	}
 
-	// protected function valid_session() {
-	// 	return true;
-	// }
+	protected function valid_session() {
+		return true;
+	}
 
 }
-
-echo "class fine";
 
 ?>
