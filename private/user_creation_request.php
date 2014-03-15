05b1654f91aedeea06c7b2f5c3290d85ee67d248
Winston Li <Roxy@Lockheed>
2014-03-15 16:17:24 +0000

    json

--- private/user_creation_request.php
+++ private/user_creation_request.php
@@ -6,13 +6,10 @@ class UserCreationRequest extends Request {
 
 	public function request() {
 		$query = $this->db->query("SELECT count(*) FROM user.user");
-		echo "got past query\n";
 		if (!$query) {
 			return $this->error("failed");
 		}
-		echo "not null\n";
 		$result = $query->fetch_assoc();
-		echo "got result: ". $result["count(*)"] . " \n";
 
 		return $this->success($result["count(*)"]);
 	}
--- www/user_creation.php
+++ www/user_creation.php
@@ -3,10 +3,7 @@
 include_once(__DIR__ . "/../private/user_creation_request.php");
 
 $request = new UserCreationRequest();
-echo "hi\n";
 $request->request();
-echo "hi2\n";
 $request->output();
-echo "done\n";
 
 ?>
