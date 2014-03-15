5f816b80cceec564811d2e3a28a2452082ffafb7
Winston Li <Roxy@Lockheed>
2014-03-15 16:15:36 +0000

    request

--- private/user_creation_request.php
+++ private/user_creation_request.php
@@ -6,10 +6,13 @@ class UserCreationRequest extends Request {
 
 	public function request() {
 		$query = $this->db->query("SELECT count(*) FROM user.user");
+		echo "got past query\n";
 		if (!$query) {
 			return $this->error("failed");
 		}
+		echo "not null\n";
 		$result = $query->fetch_assoc();
+		echo "got result \n";
 
 		return $this->success($result["count(*)"]);
 	}
