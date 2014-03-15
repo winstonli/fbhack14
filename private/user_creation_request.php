7efb00cdb0616796d23d9773d8157180d66804e8
Winston Li <Roxy@Lockheed>
2014-03-15 16:07:36 +0000

    super bottom

--- private/user_creation_request.php
+++ private/user_creation_request.php
@@ -2,20 +2,20 @@
 
 include_once __DIR__ . "/request.php";
 
-echo "got to bottom\n";
+// echo "got to bottom\n";
 
-// public class UserCreationRequest extends Request {
+public class UserCreationRequest extends Request {
 
-// 	public function request() {
-// 		$query = $this->db->request("SELECT count(*) FROM user.user");
-// 		if (!$query) {
-// 			return this->error("failed");
-// 		}
-// 		$result = $query->fetch_assoc();
+	public function request() {
+		$query = $this->db->request("SELECT count(*) FROM user.user");
+		if (!$query) {
+			return this->error("failed");
+		}
+		$result = $query->fetch_assoc();
 
-// 		return $this->success($result["count(*)"]);
-// 	}
+		return $this->success($result["count(*)"]);
+	}
 
-// }
+}
 
 ?>
--- www/user_creation.php
+++ www/user_creation.php
@@ -2,6 +2,8 @@
 
 include_once(__DIR__ . "/../private/user_creation_request.php");
 
+echo 'super bottom';
+
 // $request = new UserCreationRequest();
 // echo 'hi';
 // $request->request();
