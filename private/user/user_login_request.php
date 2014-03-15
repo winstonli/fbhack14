45b38e0b7e1a6603cd440924cb1f0535b22a275c
Winston Li <Roxy@Lockheed>
2014-03-15 17:34:29 +0000

    sessions

--- private/user/user_login_request.php
+++ private/user/user_login_request.php
@@ -15,37 +15,32 @@ class UserLoginRequest extends Request {
 
 	public function request() {
 		/* ADD PW CHECK. */
-		echo "9\n";
+		
 		$query = $this->db->query("SELECT user_id FROM user.user WHERE username = '" .
 			$this->username . "' AND password = '" .
 			$this->password . "'");
-		echo "8\n";
+
 		if (!$query) {
 			return $this->error(NULL);
 		}
-		echo "7\n";
+
 		if (!$query->num_rows) {
 			return $this->error("invalid login");
 		}
-		echo "6\n";
 		$result = $query->fetch_assoc();
 
-		echo "5\n";
 		$query->close();
 
-		echo "4\n";
 		$user_id = $result["user_id"];
 
-		echo "3\n";
 		$session_token = md5($user_id . microtime());
 		
-		echo "2\n";
 		$query = $this->db->query(
 			"INSERT INTO user.session VALUES (NULL, " . 
 				$user_id . ", x'" . 
 				$session_token . "')"
 		);
-		echo "1\n";
+
 		if (!$query) {
 			return $this->error(NULL);
 		}
