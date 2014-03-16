7175ef7317d5896d8ab7523697368d664318b49a
Winston Li <Roxy@Lockheed>
2014-03-16 01:45:50 +0000

    set cookie

--- www/js/profile.js
+++ www/js/profile.js
@@ -2,10 +2,10 @@ window.onload = function() {
 	alert("setting");
 };
 
-var expiration_date = new Date();
-var cookie_string = '';
-expiration_date.setFullYear(expiration_date.getFullYear() + 1);
-// Build the set-cookie string:
-cookie_string = "test_cookies=true; path=/; expires=" + expiration_date.toGMTString();
-// Create/update the cookie:
-document.cookie = cookie_string;
+function setSessionToken(sessionToken) {
+	var expiration_date = new Date();
+	var cookie_string = '';
+	expiration_date.setFullYear(expiration_date.getFullYear() + 1);
+	cookie_string = "session_token=fede1f5ee0d30be9e81a49f36be19de1; path=/; expires=" + expiration_date.toGMTString();
+	document.cookie = cookie_string;
+}
