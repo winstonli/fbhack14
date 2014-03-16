edce57430c99f3360efea2815bd052ea9bd70a1a
Winston Li <Roxy@Lockheed>
2014-03-16 07:20:06 +0000

    fix

--- www/api/user/get.php
+++ www/api/user/get.php
@@ -1,6 +1,6 @@
 <?php
 
-include_once __DIR__ . "/../../../private/user/user_login_request.php";
+include_once __DIR__ . "/../../../private/user/user_get_request.php";
 
 $request = new UserGetRequest($_POST["session_token"],
 							  $_POST["target_user_id"]);
