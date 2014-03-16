23d6ca152d2b4b968e887ba81285a24c0d3c5275
Winston Li <Roxy@Lockheed>
2014-03-16 10:01:19 +0000

    cock

--- www/index.php
+++ www/index.php
@@ -65,8 +65,8 @@ font-family: 'Open Sans', sans-serif;
 <!-- <div class="bcg" data-0="background-color:rgb(1,27,59);" data-top="background-color:(0,0,0);" data-anchor-target="#slide-2"> -->
 <div class="hsContainer">
 <div class="hsContent">
-<h2 data-center="opacity: 1" data--200-bottom="opacity: 0" data-206-top="opacity: 1" data-106-top="opacity: 0" data-anchor-target="#slide-2 h2">asd</h2>
-<p data-center="opacity: 1" data--200-bottom="opacity: 0" data-206-top="opacity: 1" data-106-top="opacity: 0" data-anchor-target="#slide-2 h2">Text is fading in at 206 pixels from the bottom and fading out 106 pixels from the top. <br />Next slide fades in from black.</p>
+<h2 data-center="opacity: 1" data--200-bottom="opacity: 0" data-206-top="opacity: 1" data-106-top="opacity: 0" data-anchor-target="#slide-2 h2"><!-- asdf --></h2>
+<p data-center="opacity: 1" data--200-bottom="opacity: 0" data-206-top="opacity: 1" data-106-top="opacity: 0" data-anchor-target="#slide-2 h2"><!-- Text is fading in at 206 pixels from the bottom and fading out 106 pixels from the top. <br />Next slide fades in from black. --></p>
 </div>
 </div>
 </div>
@@ -83,7 +83,7 @@ data-anchor-target="#slide-3"
 <div class="bcg bcg2" data-bottom-top="opacity: 1" data-top="opacity: 1;" data--150-top="opacity: 0" data-anchor-target="#slide-3">&nbsp;</div>
 <div class="hsContent">
 <div class="plaxEl" data-bottom-top="opacity: 0" data-top="opacity: 0" data--50p-top="opacity: 1; position: fixed; top: 206px; width: 100%; left: 0;" data--25p-bottom="opacity: 1;" data--50p-bottom="opacity: 0;" data-anchor-target="#slide-3">
-<h2>Fixed element fading in and out</h2>
+<h2><!-- Fixed element fading in and out --></h2>
 </div>
 </div>
 </div>
@@ -97,7 +97,7 @@ data-anchor-target="#slide-3"
 
 <div class="curtain" data-bottom-top="opacity: 0; height: 1%;" data-106-top="height: 1%; opacity: 0; top: -10%;" data-center="height: 100%; opacity: 0.5; top: 0%;" data-top-bottom="height: 100%; opacity: 0.5; top: 0%;" data-anchor-target="#slide-4"></div>
 <div class="copy" data-bottom-top="opacity: 0" data--100-bottom="opacity: 0" data--280-bottom="opacity: 1;" data-280-top="opacity: 1;" data-106-top="opacity: 0;" data-anchor-target="#slide-4 .copy">
-<h2>Curtain effect while you scroll</h2>
+<h2><!-- Curtain effect while you scroll --></h2>
 </div>
 
 </div>
@@ -112,14 +112,14 @@ data-anchor-target="#slide-3"
 <div class="bcg bcg2" data-bottom-top="opacity: 0;" data--33p-top="opacity: 0;" data--66p-top="opacity: 1;" data-anchor-target="#slide-5">
 <div class="hsContainer">
 <div class="hsContent" data-bottom-top="opacity: 0;" data-center="opacity: 1" data-anchor-target="#slide-5">
-<h2>Fixed element fading in and out</h2>
+<h2><!-- Fixed element fading in and out --></h2>
 </div>
 </div>
 </div>
 <div class="bcg bcg3" data-300-bottom="opacity: 0;" data-100-bottom="opacity: 1;" data-anchor-target="#slide-5">
 <div class="hsContainer">
 <div class="hsContent" data-100-bottom="opacity: 0;" data-bottom="opacity: 1;" data-anchor-target="#slide-5">
-<h2>The End</h2>
+<h2><!-- The End --></h2>
 </div>
 </div>
 </div>
