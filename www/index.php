663f20876d63ed2aa4a562475867fb2628afdfea
dkn12 <dkn12@ic.ac.uk>
2014-03-16 04:02:04 +0000

    add login form

--- www/index.php
+++ www/index.php
@@ -102,11 +102,8 @@ FB.init(
 			    		<div class="hsContent" data-center="opacity: 1" data-106-top="opacity: 0" data-anchor-target="#slide-1 h2">
 				    		<h2>Login</h2>
 							<div id="div1" style = "border:2px solid #a1a1a1;background:#dddddd;border-radius:25px;">
-								<!-- <button>User Login</button> -->					 
-								<div class="container">
-									<input type='submit' name='User Login' value='User Login' />
-									<fb:login-button show-faces="false" width="200" max-rows="1" data-auto-logout-link="true"></fb:login-button>
-								</div>
+								<button>User Login</button>					 
+								<fb:login-button show-faces="false" width="200" max-rows="1" data-auto-logout-link="true"></fb:login-button>
 							</div>
 							<div id="div2" style = "border:2px solid #a1a1a1;background:#dddddd;border-radius:25px;">
 								<div id='fg_membersite'>
@@ -137,19 +134,21 @@ FB.init(
 									</fieldset>
 									</form>
 									<script type='text/javascript'>
-// <![CDATA[
+									// <![CDATA[
 
-    var frmvalidator  = new Validator("login");
-    frmvalidator.EnableOnPageErrorDisplay();
-    frmvalidator.EnableMsgsTogether();
+										var frmvalidator  = new Validator("login");
+										frmvalidator.EnableOnPageErrorDisplay();
+										frmvalidator.EnableMsgsTogether();
 
-    frmvalidator.addValidation("username","req","Please provide your username");
+										frmvalidator.addValidation("username","req","Please provide your username");
     
-    frmvalidator.addValidation("password","req","Please provide the password");
+										frmvalidator.addValidation("password","req","Please provide the password");
 
-// ]]>
-</script>
+									// ]]>
+									</script>
 								</div>
+								
+								
 							</div>
 						</div>
 		        	</div>
