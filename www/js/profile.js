7e1584d8329fd8644035a32085600e8eceb6c4b8
Winston Li <Roxy@Lockheed>
2014-03-16 01:47:10 +0000

    Ses token

--- www/js/profile.js
+++ www/js/profile.js
@@ -1,11 +1,12 @@
 window.onload = function() {
-	alert("setting");
+	setSessionToken("fede1f5ee0d30be9e81a49f36be19de1");
+
 };
 
 function setSessionToken(sessionToken) {
 	var expiration_date = new Date();
 	var cookie_string = '';
 	expiration_date.setFullYear(expiration_date.getFullYear() + 1);
-	cookie_string = "session_token=fede1f5ee0d30be9e81a49f36be19de1; path=/; expires=" + expiration_date.toGMTString();
+	cookie_string = "session_token=" + sessionToken + "; path=/; expires=" + expiration_date.toGMTString();
 	document.cookie = cookie_string;
 }
