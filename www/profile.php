516e70a417cb0c96bc8b963c6f0f6f0bc5611ff4
Winston Li <Roxy@Lockheed>
2014-03-16 01:26:41 +0000

    Removed stupid line

--- www/profile.php
+++ www/profile.php
@@ -1,9 +1,8 @@
 <!DOCTYPE HTML>
 <head>
 <title>Playlist</title>
-<!-- <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <link href="css/main.css" rel="stylesheet" type="text/css" media="all"/>
-<script type="text/javascript" src="js/jquery.js" /> -->
+<script type="text/javascript" src="js/jquery.js" />
 
 </head>
 <body>
