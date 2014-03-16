7b2accb2656db343a09706b272067dd8ee45a5cc
Milka Horozova <falkeura@gmail.com>
2014-03-16 02:01:56 +0000

    trying to implement the facebook login button

--- www/index.php
+++ www/index.php
@@ -25,6 +25,43 @@
 <body class="loading">
 <script src="http://connect.facebook.net/en_US/all.js"></script>
 <div id="fb-root"></div>
+
+<script>
+  window.fbAsyncInit = function() {
+FB.init(
+{
+    appId  : 641083209274716,
+    status : true, // check login status
+    cookie : true, // enable cookies to allow the server to access the session
+    xfbml  : true  // parse XFBML
+});
+
+  FB.Event.subscribe('auth.authResponseChange', function(response) {
+    if (response.status === 'connected') {
+      testAPI();
+    } else if (response.status === 'not_authorized') {
+      FB.login();
+    } else {
+      FB.login();
+    }
+  });
+  };
+
+  (function(d){
+   var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
+   if (d.getElementById(id)) {return;}
+   js = d.createElement('script'); js.id = id; js.async = true;
+   js.src = "//connect.facebook.net/en_US/all.js";
+   ref.parentNode.insertBefore(js, ref);
+  }(document));
+
+  function testAPI() {
+    console.log('Welcome!  Fetching your information.... ');
+    FB.api('/me', function(response) {
+      console.log('Good to see you, ' + response.name + '.');
+    });
+
+</script>
 			
        	<main>	
 	        <section id="slide-1" class="homeSlide">
