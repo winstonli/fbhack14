f04bcbcb2c01e89a0054320cd4b489b65260f306
Winston Li <Roxy@Lockheed>
2014-03-16 01:07:16 +0000

    done

--- www/profile.php
+++ www/profile.php
@@ -2,7 +2,7 @@
 <head>
 <title>Playlist</title>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<link href="web/css/style.css" rel="stylesheet" type="text/css" media="all"/>
+<link href="css/main.css" rel="stylesheet" type="text/css" media="all"/>
 <script type="text/javascript" src="js/jquery.js" />
 
 </head>
