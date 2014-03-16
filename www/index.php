59563db558c70c7b19d441215d44729b6ad942ef
dkn12 <dkn12@ic.ac.uk>
2014-03-16 04:57:33 +0000

    add login form

--- www/index.php
+++ www/index.php
@@ -1,3 +1,15 @@
+<?PHP
+require_once("./include/membersite_config.php");
+
+if(isset($_POST['submitted']))
+{
+   if($fgmembersite->Login())
+   {
+        $fgmembersite->RedirectToURL("login-home.php");
+   }
+}
+
+?>
 <!DOCTYPE html>
 <html>
 <head>
