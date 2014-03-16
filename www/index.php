3fdfab7acc8014a3903ae71b18ea725264346e3b
dkn12 <dkn12@ic.ac.uk>
2014-03-16 01:24:14 +0000

    add login form

--- www/index.php
+++ www/index.php
@@ -31,7 +31,7 @@
 		        	<div class="hsContainer">
 			    		<div class="hsContent" data-center="opacity: 1" data-106-top="opacity: 0" data-anchor-target="#slide-1 h2">
 				    		<h2>Login</h2>
-							<div style="border:2px solid #a1a1a1;background:#dddddd;border-radius:25px;">key</div>	
+							<div class="loginForm">key</div>	
 							<div id="fb-root"></div>
 							<fb:login-button show-faces="true" width="200" max-rows="1" data-auto-logout-link="true"></fb:login-button>
 			    		</div>
