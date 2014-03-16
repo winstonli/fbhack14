a6f2c8c8aede01dbeb81f0b3561d4f97ed570fe9
aicioara <primulandrei@yahoo.com>
2014-03-16 04:35:33 +0000

    get profile token

--- www/js/profile.js
+++ www/js/profile.js
@@ -97,14 +97,17 @@ function setSessionToken(sessionToken) {
 }
 
 function getSessionToken() {
-	var parts = document.cookie.split(/;\s*/);
-	for (var i = 0; i < parts.length; i++) {
-		var part = parts[i];
-		if (part.indexOf("session_token=") == 0) {
-			return part.substring("session_token=".length)
-		}
-	}
-	return null;
+
+	return getCookie(session_token);
+
+	// var parts = document.cookie.split(/;\s*/);
+	// for (var i = 0; i < parts.length; i++) {
+	// 	var part = parts[i];
+	// 	if (part.indexOf("session_token=") == 0) {
+	// 		return part.substring("session_token=".length)
+	// 	}
+	// }
+	// return null;
 }
 
 function renderOwnPlaylist() {
