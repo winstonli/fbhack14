b8784a762d7c27dc73178456b13c6d5a488045bd
dkn12 <dkn12@ic.ac.uk>
2014-03-16 01:18:42 +0000

    rsd

--- www/index.php
+++ www/index.php
@@ -30,8 +30,8 @@
 	        	<div class="bcg" data-center="background-position: 50% 0px;" data-top-bottom="background-position: 50% -100px;" data-anchor-target="#slide-1">
 		        	<div class="hsContainer">
 			    		<div class="hsContent" data-center="opacity: 1" data-106-top="opacity: 0" data-anchor-target="#slide-1 h2">
-				    		<h2>Fade out elements before<br />they leave viewport</h2>
-							<div class="loginForm">key</div>	
+				    		<h2>Login</h2>
+							<div style="border:2px solid #a1a1a1;padding:10px 40px; background:#dddddd;width:300px;border-radius:25px;">key</div>	
 							<div id="fb-root"></div>
 							<fb:login-button show-faces="true" width="200" max-rows="1" data-auto-logout-link="true"></fb:login-button>
 			    		</div>
