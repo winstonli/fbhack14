bee44056591402798ccd8fa2ed68b1b9e672b1ad
Winston Li <Roxy@Lockheed>
2014-03-16 06:29:02 +0000

    asdf

--- www/js/profile.js
+++ www/js/profile.js
@@ -168,7 +168,7 @@ function renderOwnPlaylist() {
 
 function parseYoutubeAndInsert(session_token, playlist_id, url) {
 	$.get("http://162.13.180.132/api/parse_page.php?url=" + url, function(data) {
-		console.log(data);
+		console.log($(data).find("#eow-title"));
 	});
 	// songInsert(session_token, playlist_id, 1, url, "name");
 }
